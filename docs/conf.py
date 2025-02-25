fd9237cd0347ab8206ff1f048549f328b3bb5cc0
fakeme-jalonso <jaimealonso9712@gmail.com>
2025-02-25 23:05:52 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,16 @@
 # -- Project information ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-  
-project = 'Testing read the docs'
-copyright = '2023, Ana Caceres & Maria Guerra'
-author = 'Ana Caceres & Maria Guerra'
+
+project = 'Pycoffee abour Github and Readthedocs'
+author = 'fakeme-jalonso'
   
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-  
+
 extensions = []
   
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
   
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-   
+
 html_theme = 'alabaster'
 html_static_path = ['_static']
