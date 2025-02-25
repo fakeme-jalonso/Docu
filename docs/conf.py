b69917f7bd4665cd18ef361e7dbe8267534515fa
fakeme-jalonso <jaimealonso9712@gmail.com>
2025-02-25 22:54:50 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,20 @@
- # -- Project information -----------------------------------------------------
-  # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Project information ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
   
-  project = 'Testing read the docs'
-  copyright = '2023, Ana Caceres & Maria Guerra'
-  author = 'Ana Caceres & Maria Guerra'
+project = 'Testing read the docs'
+copyright = '2023, Ana Caceres & Maria Guerra'
+author = 'Ana Caceres & Maria Guerra'
   
-  # -- General configuration ---------------------------------------------------
-  # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
   
-  extensions = []
+extensions = []
   
-  templates_path = ['_templates']
-  exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
   
-  # -- Options for HTML output -------------------------------------------------
-  # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-  
-  html_theme = 'alabaster'
-  html_static_path = ['_static']
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+   
+html_theme = 'alabaster'
+html_static_path = ['_static']
